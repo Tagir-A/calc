0ed132432fff25503222c5e98d35ec550f64cdc9
Almasov Tagir <tagir-a@Almasovs-MacBook-Pro.local>
2017-11-07 10:29:14 +0300

    add some operations

--- bundle.js
+++ bundle.js
@@ -3,19 +3,63 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const screen = document.querySelector('.screen')
     let input = 0
     let prevInput = 0
+    let action = null
+
+    function substract(a,b) {
+        console.log(a)
+        console.log(b)
+        return a - b
+    }
 
     function setScreenValue(value) {
         const currentValue = screen.innerHTML
         currentValue.startsWith('0') && currentValue.length == 1 && value != '.' ?
-            screen.innerHTML = value
-            : screen.innerHTML += value
+            screen.innerHTML = value :
+            screen.innerHTML += value
+    }
+    function resetScreenValue() {
+        screen.innerHTML = 0
+    }
+    function changeSign() {
+        screen.innerHTML *= -1
+    }
+    function makePercent() {
+        const currentValue = screen.innerHTML
+        const {length: currentLength} = currentValue
+        const afterDivision = currentValue / 100
+        afterDivision.toString(10).length - currentLength > 3 ?
+            screen.innerHTML = Number(afterDivision.toFixed(currentLength + 2)) :
+            screen.innerHTML = afterDivision
+    }
+    function executeAction() {
+        input = screen.innerHTML
+        const value = action == null ? null :
+                        action == "-" ? substract(prevInput, input) :
+                        console.log('smth')
+        screen.innerHTML = value
+    }
+    function addAction(value) {
+        action = value
+        prevInput = screen.innerHTML
+        screen.innerHTML = 0
     }
     setScreenValue(input)
 
 
     calc.addEventListener('click', ({target}) => {
-        const value = target.getAttribute('data-value')
-        value.match(/\d/) || value.match(/\./) ? setScreenValue(value) : console.log('false')
-        console.log('a')
+        if (target.hasAttribute('data-value')) {
+            const value = target.getAttribute('data-value')
+            value.match(/\d/) || value.match(/\./) ? setScreenValue(value) :
+            value.includes('AC') ? resetScreenValue() :
+            value.includes('+/-') ? changeSign() :
+            value.includes('%') ? makePercent() :
+            value.includes('=') ? executeAction() :
+            value.includes('+') ||
+            value.includes('-') ? addAction(value) :
+            console.log('false')
+        }
+        console.log(screen.innerHTML)
+        console.log(typeof screen.innerHTML)
+        console.log('end')
     })
 });
